a01a6fedd4d862f39298379a1a633be37dcbcd7c
natiman58 <natiman58@gmail.com>
2022-09-13 16:13:35 +0300

    all rights

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,10 +1,20 @@
 -- Creates a database named hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
--- create a user at local host and grant all permissions on this db
-GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- Create user named 'hbnb_dev_db'
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
 
--- grant SELECT privilege on the databse performance_schema
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+-- Set password for the user
+SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
 
+-- grant usage for everything on the db and tables(*.*)
+GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
+
+-- grant the user only select previlege on the 'performance_schema' db
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+
+-- grant all previleges for the hbnb_dev_db only.
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+
+-- fulush changes
 FLUSH PRIVILEGES;
