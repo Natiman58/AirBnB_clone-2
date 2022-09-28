e58e0ddab7ddf34548e5cf762e6726cd73bb5e02
Natiman58 <natiman58@gmail.com>
2022-09-28 17:56:10 +0000

    set up servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,26 +1,14 @@
 #!/usr/bin/env bash
-
 # install nginx iff not installed already
+
 sudo apt-get -y update
 sudo apt-get -y install nginx
 sudo ufw allow 'Nginx HTTP'
-
-# make dir called data
 mkdir -p /data/
-
-# create the folder web_static
 mkdir -p /data/web_static/
-
-# add the dir release
 mkdir -p /data/web_static/releases/
-
-# create the folder shared
 mkdir -p /data/web_static/shared/
-
-# create the folder called test
 mkdir -p /data/web_static/releases/test/
-
-# create a fake HTML file that prints Holberton School
 echo "<html>
   <head>
   </head>
@@ -28,14 +16,8 @@ echo "<html>
     Holberton School
   </body>
 </html> " > /data/web_static/releases/test/index.html
-
-# create a symbolik b/n current and test 
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# give ownership of the /data/ folder to ubuntu
 sudo chown -R ubuntu:ubuntu /data/
-
-# update the config of nginx to serve the content of /data/web_static/current/ to hbnb_static
 sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 service nginx restart
 exit 0
