5e7969a5451b0725613cf84ec62081bd2fd3e2db
Natiman58 <natiman58@gmail.com>
2022-09-29 11:05:09 +0000

    try

---
+++ 1-pack_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# A fabric script that generates wer_static.tgz archive using the do_pack fun
+
+from fabric.operations import local
+from datetime import datetime
+
+def do_pack():
+    """
+        To mkae compressed file in the /versions/web_static dir from the web_static dir
+    """
+    time = datetime.now().strftime("%Y%m%d%H%M%S")
+    local('sudo mkdir -p versions')
+
+    archive_path = 'versions/web_static_{}'.format(time)
+
+    file = local('sudo tar -czvf {}.tgz web_static'.format(archive_path), capture=True)
+
+    if file:
+        return file
+    else:
+        return None
+
