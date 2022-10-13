b9c4defb2c3d7adabcb005a75a37b70269240329
natiman58 <natiman58@gmail.com>
2022-10-13 19:05:28 +0300

    dynamic text on flask

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+# A python script that starts a flask app
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    """
+        Displays HEllo HBNB!
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    """
+        Displays HBNB
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """
+        Displays "C" followed by the value of <text>
+        replace '_' with ' '
+    """
+    return "C {}".format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
