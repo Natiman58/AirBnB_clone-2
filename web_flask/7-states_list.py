267f70bcfefa24849a20383fb2b3aebe3938fb1b
natiman58 <natiman58@gmail.com>
2022-10-16 15:29:12 +0300

    pycode

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -11,13 +11,13 @@ from flask import render_template
 app = Flask(__name__)
 
 
-
 @app.route('/states_list', strict_slashes=False)
 def states():
     """
         Displays HTML page of all the states in the storage
     """
     return render_template('7-states_list.html', states=storage.all('State'))
+
 
 @app.teardown_appcontext
 def teardown_db(self):
@@ -26,6 +26,6 @@ def teardown_db(self):
     """
     storage.close()
 
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
-
