2af795709e7f99d1a3f6a86e96d715c1c16ecc11
Natiman58 <natiman58@gmail.com>
2022-11-07 23:36:56 +0300

    fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-@app.route('/airbnb-onepage/', strict_slashes=False)
+#@app.route('/airbnb-onepage/', strict_slashes=False)
 
 def hello_HBNB():
     """ A function that displays Hello HBNB"""
